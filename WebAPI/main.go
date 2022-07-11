33063863cca2b94fab0a7bd7b79e7870db9b283e
HankGuo <shank2288s@gmail.com>
2022-07-11 18:51:30 +0800

    feat : main.go implements LoginAuth()

--- WebAPI/main.go
+++ WebAPI/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -23,7 +24,40 @@ func LoginPage(c *gin.Context) {
 }
 
 func LoginAuth(c *gin.Context) {
+	var (
+		username string
+		password string
+	)
 
+	if in, isExist := c.GetPostForm("username"); isExist && in != "" {
+		username = in
+	} else {
+		c.HTML(http.StatusBadRequest, "login.html", gin.H{
+			"error": errors.New("必須輸入使用者名稱"),
+		})
+		return
+	}
+
+	if in, isExist := c.GetPostForm("password"); isExist && in != "" {
+		password = in
+	} else {
+		c.HTML(http.StatusBadRequest, "login.html", gin.H{
+			"error": errors.New("必須輸入密碼名稱"),
+		})
+		return
+	}
+
+	if err := Auth(username, password); err == nil {
+		c.HTML(http.StatusOK, "login.html", gin.H{
+			"success": "登入成功",
+		})
+		return
+	} else {
+		c.HTML(http.StatusUnauthorized, "login.html", gin.H{
+			"error": err,
+		})
+		return
+	}
 }
 
 func main() {
@@ -32,5 +66,6 @@ func main() {
 	server.Static("/assets", "./template/assets")
 	server.GET("/", test)
 	server.GET("/login", LoginPage)
+	server.POST("/login", LoginAuth)
 	server.Run("0.0.0.0:8080")
 }
